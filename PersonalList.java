1c4184e15de7e199287da055ec4829158b0e8749
Rajeev Ranjan <90rajeevmymail@gmail.com>
2025-10-05 22:26:18 +0530

    Update PersonalList.java

--- PersonalList.java
+++ PersonalList.java
@@ -1,15 +1,19 @@
+// Simple console-based Personal List
 import java.util.ArrayList;
 import java.util.Scanner;
 class PersonalList
   {
+    // Declaring ArrayLisg and Scanner class.
     static ArrayList<String> list=new ArrayList<String>();
     static Scanner sc=new Scanner(System.in);
+    // Method for adding values.
     public static void add()
     {
       System.out.print("Enter: ");
       String value=sc.nextLine();
       list.add(value);
     }
+    // Method for removing values.
     public static void remove()
     {
       for(int i=0; i<list.size(); i++) {
@@ -20,6 +24,7 @@ class PersonalList
       list.remove(remove-1);
       System.out.println(remove+" has been removed!");
     }
+    // Method for changing values.
     public static void change()
     {
       for(int i=0; i<list.size(); i++) {
@@ -32,15 +37,18 @@ class PersonalList
       list.set(idx-1, change);
       System.out.println("Changed Successfully!");
     }
+    // Method for showing all values one-by-one.
     public static void show()
     {
       for(int i=0; i<list.size(); i++) {
         System.out.println((i+1)+". "+list.get(i));
       }
     }
+    // Main method
     public static void main(String[] args)
     {
       System.out.println("--- Personal List ---");
+      // Menu
       System.out.print("1. Add\n2. Remove\n3. Change\n4. Show List\n5. Exit");
       int choice=0;
       while(true) {
